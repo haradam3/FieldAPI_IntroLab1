8d1c21674bc02d7bdb0a50b765265a286f825d13
haradam3 <mikako.harada@autodesk.com>
2017-12-30 02:27:26 -0500

    Initial commit

---
+++ HelloFieldAPI/FieldUI.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace HelloFieldAPI
+{
+    partial class FieldUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUserName = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelRequest = new System.Windows.Forms.Label();
+            this.labelResponse = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxRequest = new System.Windows.Forms.TextBox();
+            this.textBoxResponse = new System.Windows.Forms.TextBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelUserName
+            // 
+            this.labelUserName.AutoSize = true;
+            this.labelUserName.Location = new System.Drawing.Point(20, 20);
+            this.labelUserName.Name = "labelUserName";
+            this.labelUserName.Size = new System.Drawing.Size(79, 17);
+            this.labelUserName.TabIndex = 0;
+            this.labelUserName.Text = "User Name";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(20, 60);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(69, 17);
+            this.labelPassword.TabIndex = 1;
+            this.labelPassword.Text = "Password";
+            // 
+            // labelRequest
+            // 
+            this.labelRequest.AutoSize = true;
+            this.labelRequest.Location = new System.Drawing.Point(20, 140);
+            this.labelRequest.Name = "labelRequest";
+            this.labelRequest.Size = new System.Drawing.Size(61, 17);
+            this.labelRequest.TabIndex = 2;
+            this.labelRequest.Text = "Request";
+            // 
+            // labelResponse
+            // 
+            this.labelResponse.AutoSize = true;
+            this.labelResponse.Location = new System.Drawing.Point(20, 235);
+            this.labelResponse.Name = "labelResponse";
+            this.labelResponse.Size = new System.Drawing.Size(72, 17);
+            this.labelResponse.TabIndex = 3;
+            this.labelResponse.Text = "Response";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(123, 100);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(90, 25);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(110, 20);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(230, 22);
+            this.textBoxUserName.TabIndex = 5;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(110, 60);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(230, 22);
+            this.textBoxPassword.TabIndex = 6;
+            // 
+            // textBoxRequest
+            // 
+            this.textBoxRequest.Location = new System.Drawing.Point(20, 160);
+            this.textBoxRequest.Multiline = true;
+            this.textBoxRequest.Name = "textBoxRequest";
+            this.textBoxRequest.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxRequest.Size = new System.Drawing.Size(430, 72);
+            this.textBoxRequest.TabIndex = 7;
+            // 
+            // textBoxResponse
+            // 
+            this.textBoxResponse.Location = new System.Drawing.Point(20, 255);
+            this.textBoxResponse.Multiline = true;
+            this.textBoxResponse.Name = "textBoxResponse";
+            this.textBoxResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResponse.Size = new System.Drawing.Size(430, 220);
+            this.textBoxResponse.TabIndex = 8;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(110, 235);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(0, 17);
+            this.labelStatus.TabIndex = 9;
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Enabled = false;
+            this.buttonLogout.Location = new System.Drawing.Point(232, 100);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(90, 25);
+            this.buttonLogout.TabIndex = 10;
+            this.buttonLogout.Text = "Logout";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // FieldUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 505);
+            this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.textBoxResponse);
+            this.Controls.Add(this.textBoxRequest);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.labelResponse);
+            this.Controls.Add(this.labelRequest);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUserName);
+            this.Name = "FieldUI";
+            this.Text = "Field API Intro";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FieldUI_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUserName;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelRequest;
+        private System.Windows.Forms.Label labelResponse;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxRequest;
+        private System.Windows.Forms.TextBox textBoxResponse;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonLogout;
+    }
+}
+
---
+++ HelloFieldAPI/FieldUI.cs
@@ -0,0 +1,124 @@
+﻿#region Copyright
+//
+// Copyright (C) 2015-2017 by Autodesk, Inc.
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+//
+// Use, duplication, or disclosure by the U.S. Government is subject to
+// restrictions set forth in FAR 52.227-19 (Commercial Computer
+// Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
+// (Rights in Technical Data and Computer Software), as applicable.
+//
+// Written by M.Harada
+// 
+#endregion // Copyright
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Added for RestSharp. 
+using RestSharp;
+using RestSharp.Deserializers;
+
+namespace HelloFieldAPI
+{
+    public partial class FieldUI : Form
+    {
+        // Save ticket. 
+        private static string m_ticket = "";
+
+        public FieldUI()
+        {
+            InitializeComponent();
+        }
+
+        private void FieldUI_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (m_ticket != "")
+            {
+                bool result = Field.Logout(m_ticket);
+            }
+        }
+
+        //=========================================================
+        //  Login/Logout 
+        //=========================================================
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            // Get the user name and password from the user. 
+            string userName = textBoxUserName.Text;
+            string password = textBoxPassword.Text;
+
+            InitRequestResponse(); 
+            this.Update();
+
+            // Here is the main part that we call Field API to login 
+            m_ticket = Field.Login(userName, password);
+
+            // If success, change the button to logout.
+            if (m_ticket != null && m_ticket.Length > 0)
+            {
+                buttonLogin.Enabled = false;
+                buttonLogout.Enabled = true; 
+            }
+
+            // For our learning, 
+            // show the request and response in the form. 
+            ShowRequestResponse(); 
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            InitRequestResponse();
+            this.Update();
+
+            // Here is the main call to Field API. 
+            bool result = Field.Logout(m_ticket);
+
+            m_ticket = ""; 
+            buttonLogin.Enabled = true;
+            buttonLogout.Enabled = false;
+
+            // For our learning, 
+            // show the request and response in the form. 
+            ShowRequestResponse();
+        }
+
+        //==========================================================
+        // Helper functions 
+        //==========================================================
+        private void InitRequestResponse()
+        {
+            // initialize the request and response text in the form.
+            textBoxRequest.Text = "Request comes here";
+            labelStatus.Text = ""; 
+            textBoxResponse.Text = "Response comes here. This may take seconds. Please wait...";
+        }
+        private void ShowRequestResponse()
+        {
+            // show the request and response in the form. 
+            IRestResponse response = Field.m_lastResponse;
+            textBoxRequest.Text = response.ResponseUri.AbsoluteUri;
+            labelStatus.Text = "Status: " + response.StatusCode.ToString(); 
+            textBoxResponse.Text = response.Content;
+        }
+
+    }
+}
